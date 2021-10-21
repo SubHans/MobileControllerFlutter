0e9f5e9ebedd93d4279528ec1233d8e7e2749a85
SubHans <56150593+SubHans@users.noreply.github.com>
2021-10-21 16:09:45 +0700

    Home Page
    Add Homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,9 +8,53 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Te-Fa App")),
-      body: const Center(
-        child: Text('My Page!'),
-      ),
+      body: SafeArea(
+          child: Container(
+              child: Column(
+        children: <Widget>[
+          Image.network(
+            'http://redaksi.pens.ac.id/wp-content/uploads/2021/01/flutter-logo-sharing.png',
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text("Mobile Controller Iot"),
+          Text("Team Teaching"),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Image.network(
+                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                width: 150,
+              ),
+              Image.network(
+                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                width: 150,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Image.network(
+                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                width: 150,
+              ),
+              Image.network(
+                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                width: 150,
+              ),
+            ],
+          )
+        ],
+      ))),
       drawer: NavDrawer(),
     );
   }
