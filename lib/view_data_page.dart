2133d81056465dfdee1dccb1732d1dbb0b633b56
SubHans <56150593+SubHans@users.noreply.github.com>
2021-10-21 17:07:31 +0700

    Update view_data_page.dart

--- lib/view_data_page.dart
+++ lib/view_data_page.dart
@@ -2,15 +2,123 @@ import 'package:flutter/material.dart';
 import 'package:mobile_controller/drawer.dart';
 
 class ViewDataPage extends StatelessWidget {
-  const ViewDataPage({ Key? key }) : super(key: key);
+  const ViewDataPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Te-Fa App")),
-      body: const Center(
-        child: Text('view Data Page!'),
-      ),
+      body: SafeArea(
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text("Realtime Sensor Reading"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Image.network(
+                        'https://instabug.com/blog/wp-content/uploads/2020/02/AppDev_Flutter-Apps.png',
+                        width: 170,
+                      ),
+                      Image.network(
+                        'https://instabug.com/blog/wp-content/uploads/2020/02/AppDev_Flutter-Apps.png',
+                        width: 170,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Image.network(
+                        'https://instabug.com/blog/wp-content/uploads/2020/02/AppDev_Flutter-Apps.png',
+                        width: 170,
+                      ),
+                      Image.network(
+                        'https://instabug.com/blog/wp-content/uploads/2020/02/AppDev_Flutter-Apps.png',
+                        width: 170,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    color: Colors.blue,
+                    child: Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text("Temperature and Humidity Sensor"),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.network(
+                              'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                              width: 120,
+                            ),
+                            Image.network(
+                              'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                              width: 120,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    color: Colors.blue,
+                    child: Column(
+                      children: <Widget>[
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Text("Light intensity and Distance Sensor"),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Image.network(
+                              'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                              width: 120,
+                            ),
+                            Image.network(
+                              'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
+                              width: 120,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ))),
       drawer: NavDrawer(),
     );
   }
