b338d8520a013aaac5099197e8e6cf2c6814725c
amelrnt <ochaarn@gmail.com>
2021-10-26 11:30:40 +0700

    add send command page detail

--- lib/send_command.dart
+++ lib/send_command.dart
@@ -7,120 +7,136 @@ class SendCommand extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Te-Fa App")),
-      body: SafeArea(
-          child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    color: Colors.amber,
-                    child: Column(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text("Led Controller"),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        const MyStatefulWidget(),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    color: Colors.yellow[400],
-                    child: Column(
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text("Fan Speed"),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        const MyStatefulWidget(),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Container(
-                        width: 170,
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        color: Colors.amber,
-                        child: Column(
-                          children: <Widget>[
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Text("Relay Controller"),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Switch(value: true, onChanged: null)
-                          ],
-                        ),
+      appBar: AppBar(
+        title: Text("Te-Fa App")
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.blue[100],
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text("Led Controller"),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const MyStatefulWidget(),
+                        ],
                       ),
-                      Container(
-                        width: 170,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        color: Colors.amber,
-                        child: Column(
-                          children: <Widget>[
-                            Align(
-                              alignment: Alignment.topLeft,
-                              child: Text("Buzzer Controller"),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Switch(value: true, onChanged: null)
-                          ],
-                        ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.blue[100],
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text("Fan Speed"),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          const MyStatefulWidget(),
+                        ],
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    color: Colors.amber,
-                    child: Column(
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Align(
-                          alignment: Alignment.topLeft,
-                          child: Text("Plain Text Command"),
+                        Container(
+                          width: 170,
+                          padding: EdgeInsets.all(10),
+                          color: Colors.blue[100],
+                          child: Column(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text("Relay Controller"),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SwitchWidget(),
+                            ],
+                          ),
                         ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                              border: UnderlineInputBorder(), hintText: 'Text'),
-                        ),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: ElevatedButton(
-                            onPressed: null,
-                            child: const Text('Send'),
+                        Container(
+                          width: 170,
+                          padding: EdgeInsets.all(10),
+                          color: Colors.blue[100],
+                          child: Column(
+                            children: <Widget>[
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Text("Buzzer Controller"),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              SwitchWidget(),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ))),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.blue[100],
+                      child: Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text("Plain Text Command"),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            height: 5 * 24.0,
+                            child: TextField(
+                              maxLines: 5,
+                              decoration: InputDecoration(
+                                hintText: "Text",
+                                fillColor: Colors.white,
+                                filled: true,
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: ElevatedButton(
+                              onPressed: (){
+
+                              },
+                              child: const Text('Send'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+            ),
+        ),
+      ),
       drawer: NavDrawer(),
     );
   }
@@ -149,6 +165,30 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           _currentSliderValue = value;
         });
       },
+    );
+  }
+}
+
+class SwitchWidget extends StatefulWidget {
+  const SwitchWidget({ Key? key }) : super(key: key);
+
+  @override
+  _SwitchWidgetState createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<SwitchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    bool isSwitched = false;
+    return Switch(
+      value: isSwitched,
+      onChanged: (value) {
+        setState(() {
+          isSwitched = value;
+        });
+      },
+      activeTrackColor: Colors.blueGrey[200],
+      activeColor: Colors.blueGrey[600],
     );
   }
 }
