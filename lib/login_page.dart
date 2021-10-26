937c8836da697c6c4b3442318289a7697cd1eab6
SubHans <56150593+SubHans@users.noreply.github.com>
2021-10-26 15:34:10 +0700

    validation login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -23,69 +23,110 @@ class LoginPage extends StatelessWidget {
                   width: 200,
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: 20,
-                  bottom: 20
-                ),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Username'
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  bottom: 20
-                ),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Password'
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(
-                      builder: (context) => new HomePage(),
-                    )
-                ) ;
-              },
-                child: const Text('Login'),
-              ),
-              InkWell(
-                onTap: (){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(
-                      builder: (context) => new RegisterPage(),
-                    )
-                  );
-                },
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Dont Have an Account? ',
-                    style: TextStyle(
-                      color : Colors.black,
-                    ),
-                    children: const <TextSpan>[
-                      TextSpan(
-                        text: 'Register', 
-                        style: TextStyle(
-                          color : Colors.blue,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              Validation(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Validation extends StatefulWidget {
+  const Validation({Key? key}) : super(key: key);
+
+  @override
+  ValidationState createState() {
+    return ValidationState();
+  }
+}
+
+class ValidationState extends State<Validation> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Username'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter username';
+                }
+                return null;
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Password'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter password';
+                }
+                return null;
+              },
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => new HomePage(),
+                      ));
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Login'),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => new RegisterPage(),
+                  ));
+            },
+            child: RichText(
+              text: TextSpan(
+                text: 'Dont Have an Account? ',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                children: const <TextSpan>[
+                  TextSpan(
+                    text: 'Register',
+                    style: TextStyle(
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
