8a36dbc852dabfb39327978d4e88c58fdcdb9c2e
amelrnt <ochaarn@gmail.com>
2021-10-26 14:31:16 +0700

    add chart to view data

--- lib/view_data_page.dart
+++ lib/view_data_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_controller/chartexample.dart';
 import 'package:mobile_controller/drawer.dart';
 
 class ViewDataPage extends StatelessWidget {
@@ -34,7 +35,7 @@ class ViewDataPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                           child: Container(
                             color: Colors.blue[50],
-                            padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                             width: 170,
                             child: Row(
                               children: <Widget>[
@@ -42,6 +43,9 @@ class ViewDataPage extends StatelessWidget {
                                   'images/thermometer.png',
                                   height: 40,
                                   width: 40,
+                                ),
+                                SizedBox(
+                                  width:5
                                 ),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -58,7 +62,7 @@ class ViewDataPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                           child: Container(
                             color: Colors.blue[50],
-                            padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                             width: 170,
                             child: Row(
                               children: <Widget>[
@@ -66,6 +70,9 @@ class ViewDataPage extends StatelessWidget {
                                   'images/humidity.png',
                                   height: 40,
                                   width: 40,
+                                ),
+                                SizedBox(
+                                  width:5
                                 ),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -90,7 +97,7 @@ class ViewDataPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                           child: Container(
                             color: Colors.blue[50],
-                            padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                             width: 170,
                             child: Row(
                               children: <Widget>[
@@ -98,6 +105,9 @@ class ViewDataPage extends StatelessWidget {
                                   'images/idea.png',
                                   height: 40,
                                   width: 40,
+                                ),
+                                SizedBox(
+                                  width:5
                                 ),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -114,7 +124,7 @@ class ViewDataPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                           child: Container(
                             color: Colors.blue[50],
-                            padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 20),
+                            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
                             width: 170,
                             child: Row(
                               children: <Widget>[
@@ -122,6 +132,9 @@ class ViewDataPage extends StatelessWidget {
                                   'images/distance.png',
                                   height: 40,
                                   width: 40,
+                                ),
+                                SizedBox(
+                                  width:5
                                 ),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -136,49 +149,46 @@ class ViewDataPage extends StatelessWidget {
                         ),
                       ],
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
                     Container(
+                      margin: EdgeInsets.only(top: 20),
                       padding: EdgeInsets.all(10),
-                      color: Colors.blue,
+                      // color: Colors.blue[200],
                       child: Column(
                         children: <Widget>[
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Text(
-                              "Temperature and Humidity Sensor",
-                              style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              ),
+                          Text(
+                            "Temperature Sensor",
+                            style: const TextStyle(
+                            fontWeight: FontWeight.bold,
                             ),
                           ),
                           SizedBox(
-                            height: 20,
+                            height: 10,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Image.network(
-                                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
-                                width: 120,
-                              ),
-                              Image.network(
-                                'https://pbs.twimg.com/profile_images/1187814172307800064/MhnwJbxw_400x400.jpg',
-                                width: 120,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
+                          LineChartSample2(),
                         ],
                       ),
                     ),
-                    SizedBox(
-                      height: 20,
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.all(10),
+                      // color: Colors.blue[200],
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "Humidity Sensor",
+                            style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          LineChartSample2(),
+                        ],
+                      ),
                     ),
                     Container(
+                      margin: EdgeInsets.only(top: 20),
                       padding: EdgeInsets.all(10),
                       color: Colors.blue,
                       child: Column(
@@ -208,15 +218,14 @@ class ViewDataPage extends StatelessWidget {
                               ),
                             ],
                           ),
-                          SizedBox(
-                            height: 20,
-                          ),
                         ],
                       ),
                     )
                   ],
-                )),
-          )),
+                )
+              ),
+          )
+      ),
       drawer: NavDrawer(),
     );
   }
